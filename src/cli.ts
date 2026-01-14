247f8ecf0505f2c714222a43310b754dda80c7b1
Jules LIBERT <jules.lib@gmail.com>
2026-01-14 18:41:13 +0100

    feat(cli): register new commands in CLI entry point

--- src/cli.ts
+++ src/cli.ts
@@ -4,8 +4,14 @@ import kleur from "kleur";
 // Commands
 import { accountsCommand } from "./commands/accounts.js";
 import { aliasCommand } from "./commands/alias.js";
+import { archiveCommand } from "./commands/archive.js";
 import { authCommand } from "./commands/auth.js";
 import { chatsCommand } from "./commands/chats.js";
+import { contactsCommand } from "./commands/contacts.js";
+import { downloadCommand } from "./commands/download.js";
+import { focusCommand } from "./commands/focus.js";
+import { messagesCommand } from "./commands/messages.js";
+import { remindersCommand } from "./commands/reminders.js";
 import { searchCommand } from "./commands/search.js";
 import { sendCommand } from "./commands/send.js";
 import { version } from "./version.js";
@@ -19,7 +25,13 @@ program
 program.addCommand(authCommand);
 program.addCommand(aliasCommand);
 program.addCommand(accountsCommand);
+program.addCommand(archiveCommand);
 program.addCommand(chatsCommand);
+program.addCommand(contactsCommand);
+program.addCommand(downloadCommand);
+program.addCommand(focusCommand);
+program.addCommand(messagesCommand);
+program.addCommand(remindersCommand);
 program.addCommand(sendCommand);
 program.addCommand(searchCommand);
 
